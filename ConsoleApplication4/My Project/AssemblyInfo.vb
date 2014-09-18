b82a020c5fe85fb34e6fdb322418b1bae1943a3d
2818229 <2818229@gmail.com>
2014-09-18 16:39:48 +0800

    aaa

---
+++ ConsoleApplication4/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleApplication4")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("City University of Hong Kong")> 
+<Assembly: AssemblyProduct("ConsoleApplication4")> 
+<Assembly: AssemblyCopyright("Copyright © City University of Hong Kong 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("966928ee-93e2-4b8d-8d50-8bc6f65afa4b")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
